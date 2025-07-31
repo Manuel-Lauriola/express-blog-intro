42cf09d3ada99e9ae470c71eed6778d0b36b3121
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-31 16:56:37 +0200

    [UPD] - define base route

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world")
+//importo il modulo express
+const express = require(`express`)
+//dichiaro il risultato della funzione nella variabile app
+const app = express()
+//definisco la porta
+const port = 3000
+//definisco la rotta base
+app.get(`/`, (req, res) => {
+  res.send(`Server del mio blog`)
+})
